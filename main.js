90f9d7128ea0d99ddc6590b0ccf5ddcd48a9c05b
Thanh An Nguyen <nthanhan0604@gmail.com>
2017-02-23 20:43:48 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(document).on("scroll", onScroll);
+
+});
+
+function onScroll(event){
+  var scrollPosition = $(document).scrollTop();
+    $('nav a').each(function () {
+      var currentLink = $(this);
+      var refElement = $(currentLink.attr("href"));
+      if (refElement.position().top <= scrollPosition 
+          && refElement.position().top + refElement.height() > scrollPosition) {
+        $('nav a').removeClass("active");
+        currentLink.addClass("active");
+      }
+      else{
+        currentLink.removeClass("active");
+      }
+    });
+}
